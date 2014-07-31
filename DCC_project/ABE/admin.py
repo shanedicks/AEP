3eef6b2b851b57821d4e35cd2e7b1d643363a472
Shane Dicks <shane.dicks1@gmail.com>
2014-07-31 02:33:29 -0500

    registered models in admin

--- DCC_project/ABE/admin.py
+++ DCC_project/ABE/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from ABE.models import Session, Skill, Pathway, Course, Teacher, Student, TABE_Score, HiSET_Practice_Score
 
-# Register your models here.
+admin.site.register(Session) 
+admin.site.register(Skill)
+admin.site.register(Pathway) 
+admin.site.register(Course) 
+admin.site.register(Teacher) 
+admin.site.register(Student) 
+admin.site.register(TABE_Score)  
+admin.site.register(HiSET_Practice_Score)
